51f16f9ab9b9b77ed38846119d491b1fe284a697
maxwo <maxwo@DESKTOP-0QABD18.home>
2022-06-11 10:23:59 +0200

    ManyToOne for Restaurants & Standort

--- ProjectFourDM/src/main/java/ch/zhaw/springboot/entities/restaurants.java
+++ ProjectFourDM/src/main/java/ch/zhaw/springboot/entities/restaurants.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 @Entity
 
@@ -17,6 +18,9 @@ public class restaurants {
 	
 	@ManyToMany (mappedBy = "matchedRestaurants")
     Set<User> matches;
+	
+	@OneToMany   
+	Set<Standort> standorte;
 	
 	private String name;
 	private long vertragsStart;
